1a866b4f81b264cad3a00baee040812fb47b7354
Davies Liu <davies.liu@gmail.com>
2015-06-20 23:28:02 -0700

    MooseFS 2.0.72

--- freebsd/makeports.sh
+++ freebsd/makeports.sh
@@ -29,7 +29,7 @@ PORTNAMES="master chunkserver client metalogger cgi cgiserv cli netdump"
 
 PORTFILES="Makefile pkg-descr pkg-plist files"
 
-VERSION=2.0.71
+VERSION=2.0.72
 RELEASE=1
 
 if [ -f "$DISTFILEBASE" ]; then
